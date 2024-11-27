fb5ef7cd697ea447d9573b258f5b9b30b597fbf7
Robert <rob76c@gmail.com>
2024-11-26 19:39:54 -0500

    Initial Commit

---
+++ ProjectSculpt Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ProjectSculpt Watch App
+//
+//  Created by Monty on 11/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectSculpt Watch App/ProjectSculptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSculptApp.swift
+//  ProjectSculpt Watch App
+//
+//  Created by Monty on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSculpt_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
